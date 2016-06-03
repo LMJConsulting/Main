264f0347199abdab2a5c23c9462d70c44ef55018
gfsmithak <gfsmithak@gmail.com>
2016-06-02 16:27:46 -0800

    Update Find local admins.ps1

--- Info-Gathering/Find local admins.ps1
+++ Info-Gathering/Find local admins.ps1
@@ -1,4 +1,3 @@
-﻿$Targets=@()
-get-adcomputer -filter * | select -expandproperty name | foreach {if ((test-connection -computername $_ -quiet) -eq $true) {$targets=$targets+$_}}
-
+﻿$Targets=@();
+get-adcomputer -filter * | select -expandproperty name | foreach {if ((test-connection -computername $_ -quiet) -eq $true) {$targets=$targets+$_}};
 foreach ($t in $targets) {$admins=gwmi win32_groupuser -computername $t | where {$_.groupcomponent -like "*Administrators*"} | select partcomponent ; $admins=$admins | select -expandproperty partcomponent | out-string ; $result=$admins | select-string '(domain.*)' -allmatches | % {$_.matches} | select value ; $result+$t | out-file  C:\test.txt -append}
